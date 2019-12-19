8dcc33aea064c1ecb74f073efcb5d0d1f030969e
Tanya Bihag <tbihag@gmail.com>
2019-12-19 03:52:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,18 +36,11 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
     shipping_manifest["muskets"] = 2,
     shipping_manifest["gun_powder"] = 4
     return shipping_manifest
     
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+ 
 
 end
