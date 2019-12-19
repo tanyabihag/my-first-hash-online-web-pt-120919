8d804939be302033d02f1d6baea284ec27f19515
Tanya Bihag <tbihag@gmail.com>
2019-12-19 03:42:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ about_me = {"age" => 21, "name" => "Tanya", "hair" => "black"}
 
   
 end
